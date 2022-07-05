cbc25ff61e9262234e1fcf8c1621f3a592697b6e
Thomas-Lacomblez <34097025+Thomas-Lacomblez@users.noreply.github.com>
2022-07-05 12:37:50 +0200

    Update gestionEcoleApplication.java

--- GE/src/main/java/eu/ensup/tlacomblez/gestionEcole/gestionEcoleApplication.java
+++ GE/src/main/java/eu/ensup/tlacomblez/gestionEcole/gestionEcoleApplication.java
@@ -2,6 +2,7 @@ package eu.ensup.tlacomblez.gestionEcole;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
