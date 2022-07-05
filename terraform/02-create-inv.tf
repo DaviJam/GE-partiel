c9c6b6f302a7f549df7e96150bef8930295d41b2
Thomas-Lacomblez <34097025+Thomas-Lacomblez@users.noreply.github.com>
2022-07-05 22:18:31 +0200

    Update 02-create-inv.tf

--- terraform/02-create-inv.tf
+++ terraform/02-create-inv.tf
@@ -2,27 +2,27 @@ resource "null_resource" "ansible-provision" {
   depends_on = [aws_instance.swarm-master, aws_instance.aws-swarm-members]
 
   provisioner "local-exec" {
-    command = "echo \"[all:vars]\" >> ./swarm-inventory"
+    command = "echo \"[all:vars]\" >> ../ansible/swarm-inventory"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${format("ansible_ssh_private_key_file=%s", var.private_key_path)}\" >>  ./swarm-inventory"
+    command = "echo \"${format("ansible_ssh_private_key_file=%s", var.private_key_path)}\" >>  ../ansible/swarm-inventory"
   }
 
   provisioner "local-exec" {
-    command = "echo \"[swarm-master]\" >> ./swarm-inventory"
+    command = "echo \"[swarm-master]\" >> ../ansible/swarm-inventory"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${format("%s ansible_ssh_user=%s", aws_instance.swarm-master.0.public_ip, var.ssh_user)}\" >> ./swarm-inventory"
+    command = "echo \"${format("%s ansible_ssh_user=%s", aws_instance.swarm-master.0.public_ip, var.ssh_user)}\" >> ../ansible/swarm-inventory"
   }
 
   provisioner "local-exec" {
-    command = "echo \"[swarm-nodes]\" >> ./swarm-inventory"
+    command = "echo \"[swarm-nodes]\" >> ../ansible/swarm-inventory"
   }
 
   provisioner "local-exec" {
-    command = "echo \"${join("\n",formatlist("%s ansible_ssh_user=%s", aws_instance.aws-swarm-members.*.public_ip, var.ssh_user))}\" >> ./swarm-inventory"
+    command = "echo \"${join("\n",formatlist("%s ansible_ssh_user=%s", aws_instance.aws-swarm-members.*.public_ip, var.ssh_user))}\" >> ../ansible/swarm-inventory"
   }
 }
  
