270170346cfb17f4be06f12a6d7569ed59867ba6
asemin08 <semin.allan@gmail.com>
2022-07-06 09:55:02 +0200

    Ajout token

--- src/test/java/eu/ensup/gestionEcole/gatling/GatlingScenario.java
+++ src/test/java/eu/ensup/gestionEcole/gatling/GatlingScenario.java
@@ -1,25 +1,72 @@
  package eu.ensup.gestionEcole.gatling;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.http;
 
+
 public class GatlingScenario extends Simulation {
 
 
+    String public_ip = System.getProperty("public_ip");
+    Map<String,String> values = new HashMap<String, String>() {{
+        put("email", "directeur@ensup.eu");
+        put ("password", "directeur");
+    }};
+    ObjectMapper objectMapper = new ObjectMapper();
+    String rbody;
+    String token = "Bearer ";
+    {
+        try {
+            rbody = objectMapper.writeValueAsString(values);
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://"+public_ip+":8080/login"))
+                    .POST(HttpRequest.BodyPublishers.ofString(rbody))
+                    .setHeader("Content-type","application/json")
+                    .build();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+            JSONObject jsonObject = new JSONObject(response.body());
+
+            token= token+jsonObject.getString("token");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
     ScenarioBuilder etudiant_api = scenario("EtudiantApi")
             .exec(http("liter tous les etudiants")
-                    .get("/api/students/getall"))
+                    .get("/api/students/getall").header("Authorization",token))
             .pause(1)
             .exec(http("get un etudiant")
-                    .get("/api/students/get/uuid1")
+                    .get("/api/students/get/uuid1").header("Authorization",token)
             )
             .pause(1)
             .exec(http("Ajouter un etudiant")
                     .post("/api/students/add")
                     .header("Content-Type", "application/json")
+                    .header("Authorization",token)
                     .body(StringBody("{" +
                             "    \"nom\": \"Ali\"," +
                             "    \"prenom\": \"Gator\"," +
@@ -33,7 +80,9 @@ public class GatlingScenario extends Simulation {
 
     ScenarioBuilder cours_api = scenario("CoursApi")
             .exec(http("lister les cours")
-                    .get("/api/course"))
+                    .get("/api/course")
+                    .header("Authorization",token)
+            )
             .pause(1);
 
 
