6f0daafbbb58a81e64ecf989f44f8fe93e835abd
asemin08 <60793285+asemin08@users.noreply.github.com>
2022-07-06 18:54:29 +0200

    Update GatlingScenario.java

--- GE/src/test/java/eu/ensup/gestionEcole/gatling/GatlingScenario.java
+++ GE/src/test/java/eu/ensup/gestionEcole/gatling/GatlingScenario.java
@@ -136,7 +136,7 @@ public class GatlingScenario extends Simulation {
                 //permet de tester 10 utilisateur sur 10 sec
 //                 directeur.injectOpen(rampUsers(10).during(60)),
                 //permet de tester 10 utilisateur en meme temps
-                directeur2.injectOpen(atOnceUsers(10))
+                directeur2.injectOpen(atOnceUsers(50))
         ).protocols(httpProtocol);
     }
 
